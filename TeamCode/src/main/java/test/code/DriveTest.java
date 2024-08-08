f9b274895413df3a6a210c2c4b8361e3ae7d1e56
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2024-08-08 12:05:15 -0700

    New manual drive logic

--- TeamCode/src/main/java/test/code/DriveTest.java
+++ TeamCode/src/main/java/test/code/DriveTest.java
@@ -9,6 +9,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
@@ -21,6 +22,7 @@ import java.io.InputStream;
 
 import common.Drive;
 import common.Logger;
+import common.PIDController;
 
 @TeleOp(name="* Drive Test", group="Test")
 @SuppressWarnings("unused")
@@ -42,7 +44,7 @@ public class DriveTest extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         drive = new Drive(this);
-        drive.start();
+        //drive.start();
 
         telemetry.addData("Status", "Press start");
         telemetry.update();
@@ -50,6 +52,8 @@ public class DriveTest extends LinearOpMode {
         waitForStart();
 
         telemetry.clear();
+
+        runTest();
 
         drive.setBraking(false);
 
@@ -63,38 +67,157 @@ public class DriveTest extends LinearOpMode {
             telemetry.addData("Inches", "%4.1f", inches);
             telemetry.addData("Drift", "%8.2f", drive.totalDrift);
             telemetry.addData("Yaw:", "%6.2f", drive.getOrientation() );
+            telemetry.addData("Odometer:", "%d", drive.odometer.getCurrentPosition());
 
             telemetry.update();
 
             if (gamepad1.y) {
-                //drive.moveDistanceWithGyro(Drive.DIRECTION.FORWARD, speed, inches, timeout);
-                while (gamepad1.y) {
-                    drive.moveRobotWithPIDControl(Drive.DIRECTION.FORWARD, speed);
-                    telemetry.addData("Drift", "%8.2f", drive.totalDrift);
-                    telemetry.addData("Yaw:", "%6.2f", drive.getOrientation() );
-                    telemetry.update();
-
-                }
-                drive.stopRobot();
+                testCorrection(inches, Drive.DIRECTION.FORWARD);
             }
 
             if (gamepad1.a) {
-                while (gamepad1.a) {
-                    drive.moveRobotWithPIDControl(Drive.DIRECTION.BACK, speed);
-                    telemetry.addData("Drift", "%8.2f", drive.totalDrift);
-                    telemetry.addData("Yaw:", "%6.2f", drive.getOrientation() );
-                    telemetry.update();
-                }
-                drive.stopRobot();
+                testCorrection(inches, Drive.DIRECTION.BACK);
             }
 
             if (gamepad1.b) {
-                testCorrection(inches);
+                testCorrection(inches, Drive.DIRECTION.LEFT);
+            }
+
+            if (gamepad1.y) {
+                testCorrection(inches, Drive.DIRECTION.RIGHT);
             }
         }
     }
 
-    private void testCorrection(double inches) {
+
+    public void runTest() {
+
+        // ToDo remove
+        boolean running = true;
+        boolean driving = false;
+        Drive.DIRECTION lastDirection = Drive.DIRECTION.STOOPED;
+        double MIN_SPEED = 0.25;
+        double MAX_SPEED = 0.9;
+        double MIN_ROTATE_SPEED = 0.25;
+        double MAX_ROTATE_SPEED = 0.50;
+        telemetry.addData("Speed", "%4.2f", speed);
+        telemetry.update();
+
+
+        while (!isStarted()) Thread.yield();
+        Logger.message("robot drive thread started");
+
+        ElapsedTime driveTime = new ElapsedTime();
+        double ACCELERATION_TIME = (1000 * 1.5);   // 1.5 second to accelerate to full speed
+        double DECELERATION_TIME = (1000 * 1.0);   // 1 second to come to full stop
+        double accelerationPerMS = 0;
+        double decelerationPerMS = 0;
+
+
+        double lastTime = driveTime.milliseconds();
+        double lastSpeed = 0;
+
+        while (running && opModeIsActive()) {
+
+            // ToDo remove, emergency stop for testing
+            if (gamepad1.back) {
+                requestOpModeStop();
+                break;
+            }
+
+            // Left stick to go forward back and strafe. Right stick to rotate. Left trigger accelerate.
+            Gamepad gamepad = gamepad1;
+            double x = -gamepad.left_stick_y;
+            double y = -gamepad.left_stick_x;
+            double yaw = -gamepad.right_stick_x;
+            double noise = 0.01;
+            double speedX = 0;
+            double speedY = 0;
+            double speedYaw = 0;
+            speed = 0;
+
+            if (Math.abs(x) > noise || Math.abs(y) > noise || Math.abs(yaw) > noise ) {
+                Drive.DIRECTION direction;
+
+                if (Math.abs(x) > noise && Math.abs(y) <= noise && Math.abs(yaw) <= noise) {
+                    if (x > 0)
+                        direction = Drive.DIRECTION.FORWARD;
+                    else
+                        direction = Drive.DIRECTION.BACK;
+                    speed = Math.pow(Math.abs(x), 3) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
+
+                } else if (Math.abs(y) > noise && Math.abs(x) <= noise && Math.abs(yaw) <= noise) {
+                    if (y > 0)
+                        direction = Drive.DIRECTION.LEFT;
+                    else
+                        direction = Drive.DIRECTION.RIGHT;
+                    speed = Math.pow(Math.abs(y), 3) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
+
+                } else if (Math.abs(yaw) > noise && Math.abs(x) <= noise && Math.abs(y) <= noise) {
+                    if (yaw > 0)
+                        direction =  Drive.DIRECTION.TURN_LEFT;
+                    else
+                        direction = Drive.DIRECTION.TURN_RIGHT;
+                    speed = Math.pow(Math.abs(yaw), 3) * (MAX_ROTATE_SPEED - MIN_ROTATE_SPEED) + MIN_ROTATE_SPEED;
+
+                } else {
+                    direction =  Drive.DIRECTION.DRIVER;
+                    if (Math.abs(x) > noise) {
+                        speedX = Math.pow(x, 3) * (MAX_SPEED - MIN_SPEED);
+                        if (x > 0)
+                            speedX += MIN_SPEED;
+                        else
+                            speedX -= MIN_SPEED;
+                    }
+                    speedY = Math.pow(y, 3) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED;
+                    speedYaw = Math.pow(yaw, 3) * (MAX_ROTATE_SPEED - MIN_ROTATE_SPEED) + MIN_ROTATE_SPEED;
+                }
+
+                // limit acceleration and deceleration to prevent skidding.
+                double currentTime = driveTime.milliseconds();
+                accelerationPerMS = (MAX_SPEED - MIN_SPEED) / ACCELERATION_TIME;
+                decelerationPerMS = (MAX_SPEED - MIN_SPEED) / DECELERATION_TIME;
+
+                double deltaTime = currentTime - lastTime;
+                double acceleration = (speed - lastSpeed) / (deltaTime);
+                if ((speed > lastSpeed) && (acceleration > (accelerationPerMS * deltaTime)))
+                    speed = lastSpeed + (accelerationPerMS * deltaTime);
+                else if ((lastSpeed > 0) && (speed < lastSpeed) && (Math.abs(acceleration) > (decelerationPerMS * deltaTime)))
+                    speed = lastSpeed - (decelerationPerMS * deltaTime);
+                lastTime = currentTime;
+                lastSpeed = speed;
+
+                if (direction == Drive.DIRECTION.DRIVER) {
+                    drive.moveRobot(speedX, speedY, speedYaw);
+                } else {
+                    drive.moveRobot(direction, speed);
+                }
+
+                driving = true;
+                lastDirection = direction;
+                Logger.message("%-12s   x: %6.2f  y: %6.2f  yaw: %6.2f  speed: %6.2f  speedX: %6.2f  speedY: %6.2f  speedYaw: %6.2f",
+                        direction, x , y, yaw, speed, speedX, speedY, speedYaw);
+
+            } else if (driving) {
+                drive.stopRobot();
+                lastDirection = Drive.DIRECTION.STOOPED;
+                driving = false;
+
+            } else {
+                Thread.yield();
+            }
+
+            telemetry.addData("Speed", "%4.2f", speed);
+            telemetry.update();
+
+        }
+        Logger.message("robot drive thread stopped");
+    }
+
+
+
+
+    private void testCorrection(double inches, Drive.DIRECTION direction) {
 
         // Disable drift correction and test how straight the drivetrain drives.
         /*
@@ -111,7 +234,9 @@ public class DriveTest extends LinearOpMode {
         */
 
         drive.resetOrientation();
-        drive.moveDistanceWithPIDControl (Drive.DIRECTION.FORWARD, speed, inches, 0);
+        drive.moveDistanceWithPIDControl (direction, speed, inches, 0);
+
+
         telemetry.addData("Yaw:", "%6.2f", drive.getOrientation() );
         telemetry.update();
     }
@@ -123,6 +248,24 @@ public class DriveTest extends LinearOpMode {
         timeout = Math.max(timeout, 0);
     }
 
+    private void testPID() {
+
+        double PID_DRIVE_KP = 0.02;
+        double PID_DRIVE_KI = 0;
+        double PID_DRIVE_KD = 0;
+
+        PIDController pidDrive = new PIDController(PID_DRIVE_KP, PID_DRIVE_KI, PID_DRIVE_KD);
+
+        pidDrive.setSetpoint(0);
+        pidDrive.setOutputRange(PID_DRIVE_KP*3, PID_DRIVE_KP*3);   //
+        pidDrive.setInputRange(-12, 12);
+        pidDrive.enable();
+
+        double correction = pidDrive.performPID(-0.5);
+
+        Logger.message("correction: %6.3f", correction);
+
+    }
 
     private void checkAccuracy () {
 
