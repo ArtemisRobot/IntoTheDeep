8646f9dd45aa628de59bb71db360e11e9712965a
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2024-11-18 17:04:37 -0800

    Initial version

---
+++ TeamCode/src/main/java/common/Auto.java
@@ -0,0 +1,28 @@
+package common;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Auto {
+
+    LinearOpMode opMode;
+    Robot robot;
+
+    public Auto(LinearOpMode opMode, Robot robot) {
+
+        this.opMode = opMode;
+        this.robot = robot;
+    }
+
+    public void scoreSample () {
+
+    }
+
+    public void pickupSample () {
+
+    }
+}
+
+
+
