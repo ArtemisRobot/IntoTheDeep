0e9b38996df62ea91e521929e5d955205a67cf1e
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2024-12-06 15:10:37 -0800

    Added drop sample commend

--- TeamCode/src/main/java/main/MainTeleOp.java
+++ TeamCode/src/main/java/main/MainTeleOp.java
@@ -36,6 +36,7 @@ import common.Robot;
                 "  b - picker open / close\n" +
                 "  dpad up - lifter up\n" +
                 "  dpad down - lifer down\n" +
+                "  dpad left or right - drop in bucket\n" +
                 "  left stick - arm manual control\n" +
                 "  right stick - lifter manual control\n" +
                 "\n");
@@ -73,24 +74,10 @@ import common.Robot;
             while (gamepad.b) sleep(10);
 
         } else if (gamepad.y) {
-
             robot.armMoveTo(robot.ARM_EXCHANGE);
             while (robot.isBusy() && opModeIsActive()) sleep(10);
             robot.pickUpSample();
-            /*
-            // move the game piece to scoring position
-            robot.pickerUp();
-            robot.dropperDown();
-            sleep(1000);         // wait for the dropper to get to the down position
-            robot.armMoveTo(robot.ARM_EXCHANGE);
-            robot.dropperClose();
-            sleep(400);          // wait for the dropper to get to the closed position
-            robot.pickerOpen();
-            sleep(400);
-            robot.dropperUp();
-            */
             while (gamepad.y) sleep(10);
-
 
         } else if (gamepad.a) {
             robot.setToPickingPosition(robot.AMR_OUT_PART_WAY);
@@ -106,6 +93,10 @@ import common.Robot;
             // lower the lifter
             robot.lifterDown();
             while (gamepad.dpad_down) sleep(10);
+
+        } else if (gamepad.dpad_left || gamepad.dpad_right) {
+            robot.dropSampleInTopBucket();
+            while (gamepad.dpad_left || gamepad.dpad_right) sleep(10);
 
         } else if (gamepad.left_stick_y < 0) {
             // retract the arm
