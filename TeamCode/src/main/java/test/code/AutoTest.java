13a9f69a2f799f32b5ec61f07152d7eeb159e4ae
artlamb <146391861+artlamb@users.noreply.github.com>
2024-12-20 17:20:26 -0800

    Added sleep

--- TeamCode/src/main/java/test/code/AutoTest.java
+++ TeamCode/src/main/java/test/code/AutoTest.java
@@ -51,6 +51,7 @@ public class AutoTest extends LinearOpMode {
                     robot.pickerDown();
                 } else {
                     robot.pickerUp();
+                while (gamepad1.right_bumper) sleep(10);
                 }
             } else if (gamepad1.left_bumper) {
                 if (robot.dropperIsUp()) {
