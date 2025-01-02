e5f1726dd08320226563e0d604d1fda97a3cd41d
delmarrobotics <145071561+delmarrobotics@users.noreply.github.com>
2025-01-02 10:01:44 -0800

    Changed to 2 motor lifter

--- TeamCode/src/main/java/test/code/LifterTest.java
+++ TeamCode/src/main/java/test/code/LifterTest.java
@@ -7,93 +7,58 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import common.Lifter;
+
 @TeleOp(name="Lifter Test")
-@Disabled
 
  public class LifterTest extends LinearOpMode {
 
-    int LIFTER_UP_POSITION = 1000;
-    int LIFTER_DOWN_POSITION = 0;
+    public static double LIFTER_SPEED = 0.50;
+    public static double LIFTER_SPEED_LOW = 0.20;
+    public static int    LIFTER_STOP_TICKS = 500;
+    public static int    LIFTER_UP_POSITION = 3228;
+    public static int    LIFTER_DOWN_POSITION = 0;
 
-    double ARM_SERVO_DOWN_POSITION = 0.880;
-    double ARM_SERVO_UP_POSITION = .210;
-
-    double LIFTER_SPEED = 0.25;
-
-    DcMotorEx lifter;
-    Servo armServo;
+    Lifter lifter;
 
     @Override
     public void runOpMode() {
         telemetry.addLine("Press Start");
         telemetry.update();
 
-        lifter = hardwareMap.get(DcMotorEx.class, "lifter");
-        lifter.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lifter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lifter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        armServo = hardwareMap.get(Servo.class, "armServo");
+        lifter = new Lifter(this);
+        lifter.setRange(LIFTER_DOWN_POSITION, LIFTER_UP_POSITION);
+        lifter.setLowSpeedThreshold(LIFTER_STOP_TICKS);
+        lifter.start();
 
         waitForStart();
-
-
 
         while (opModeIsActive()) {
 
             if (gamepad1.y) {
-                lifterUp();
+                lifter.setPosition(LIFTER_UP_POSITION, LIFTER_SPEED, LIFTER_SPEED);
                 while (gamepad1.y)
                     sleep(10);
             }
+
              if (gamepad1.a) {
-                 lifterDown();
+                 lifter.setPosition(LIFTER_DOWN_POSITION, LIFTER_SPEED, LIFTER_SPEED_LOW);
                  while (gamepad1.a)
                      sleep(10);
              }
-             if (gamepad1.x) {
-                 grabberUp();
-                 while (gamepad1.x)
+
+             else if (gamepad1.right_stick_y < 0) {
+                 lifter.runLifter(LIFTER_SPEED);
+                 while (gamepad1.right_stick_y < 0)
                      sleep(10);
+                 lifter.stopLifter();
+
+             } else if (gamepad1.right_stick_y > 0) {
+                 lifter.runLifter(-LIFTER_SPEED);
+                 while (gamepad1.right_stick_y > 0)
+                     sleep(10);
+                 lifter.stopLifter();
              }
-
-             if (gamepad1.b) {
-                 grabberDown();
-                    while (gamepad1.b)
-                        sleep(10);
-             }
         }
-    }
-
-    private void grabberUp() {
-        armServo.setPosition(ARM_SERVO_DOWN_POSITION);
-    }
-
-    private void grabberDown() {
-        armServo.setPosition(ARM_SERVO_UP_POSITION);
-    }
-
-    private void lifterUp() {
-
-        lifter.setTargetPosition(LIFTER_UP_POSITION);
-        lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lifter.setPower(LIFTER_SPEED);
-        while (opModeIsActive() && lifter.isBusy()) {
-            if (gamepad1.back)
-                break;
-        }
-        lifter.setPower(0);
-    }
-
-    private void lifterDown() {
-        lifter.setTargetPosition(LIFTER_DOWN_POSITION);
-        lifter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lifter.setPower(LIFTER_SPEED);
-        while (opModeIsActive() && lifter.isBusy()) {
-            if (gamepad1.back)
-                break;
-        }
-        lifter.setPower(0);
-
     }
 }
